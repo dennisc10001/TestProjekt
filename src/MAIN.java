20a0a54411d3ef3cc0f07526d36984a115fcd0e5
dennisc10001 <dennis.czech@t-online.de>
2012-12-19 17:39:58 +0100

    Vom Vaio

--- src/MAIN.java
+++ src/MAIN.java
@@ -8,7 +8,13 @@ public class MAIN {
 		
 		
 		int x = 99;
-		boolean Wahr=false;
+	//	boolean Wahr=false;
+		while (x==99){
+			// Das ist nur ein Test
+			System.out.println("Hello world ");
+			x=1;
+			
+		}
 		
 		// TODO Auto-generated method stub
 
